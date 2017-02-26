25efd4199036de335d50a7e777aa82d04f7842b4
ps9793 <piyush.swain3@gmail.com>
2017-02-26 14:42:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article One Served here');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article Two Served here');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article Three Served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/kit.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'kit.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
 
 
