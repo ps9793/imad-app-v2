142549b7e0fe8dd9434d03709b8850b435237b9e
ps9793 <piyush.swain3@gmail.com>
2017-03-19 08:47:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/kit.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'kit.png'));
+app.get('/ui/logo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
 
 
