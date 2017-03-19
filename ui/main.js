229b3866f6bfa41d6643c025a2d26528414b1bf3
ps9793 <piyush.swain3@gmail.com>
2017-03-19 12:02:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+//move image
+
+var img=document.getElementById("logo");
+var left=0;
+function moveRight(){
+    left=left+10;
+    img.style.marginLeft= left + "px";
+}
+
+img.onclick=function(){
+    var interval=setInterval(moveRight,100);
+};
