06afa573cd898d54ceffbcbaf64a48362129ae23
ps9793 <piyush.swain3@gmail.com>
2017-03-19 08:44:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article One Served here');
+  res.sendFile(path.join(_dirname,'ui','a1.html'));
 });
 
 app.get('/article-two', function (req, res) {
