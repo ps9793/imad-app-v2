2ad81d41aa30a43bdccfb0ff73c3b4e493e32ef8
ps9793 <piyush.swain3@gmail.com>
2017-03-23 22:46:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //move image
 
-var img=document.getElementById("logo");
+/*var img=document.getElementById("logo");
 var left=0;
 function moveRight(){
     left=left+10;
@@ -11,4 +11,26 @@ function moveRight(){
 
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
+};*/
+
+var button = document.getElementById('counter');
+button.onclick = function () {
+    
+    //Create a request
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // TAKE SOME ACTION
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //Make a request to the counter endpoint.
+    request.open('GET' , 'http://bandita19.imad.hasura-app.io/counter' , true);
+    request.send(null);
 };
