69701010fbc3d8d60490b92ee9cd80efdfa7e4ae
ps9793 <piyush.swain3@gmail.com>
2017-03-24 20:58:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,3 +34,18 @@ button.onclick = function () {
     request.open('GET' , 'http://ps9793.imad.hasura-app.io/counter' , true);
     request.send(null);
 };
+
+//Submit Name and put in Ordered List
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('btn');
+submit.onclick=function(){
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('list');
+    list.innerHTML=list;
+};
